589dde3e487de7527d4127d1c075623097beca07
Brent Gustafson <50030696+BrentG6000@users.noreply.github.com>
2023-02-13 14:30:04 -0600

    Added header img, date, css

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,14 @@
+import moment from 'moment';
 import coffeePic from '../images/coffee_cup.jpg';
-import '../css/header.css';
+import '../css/header.scss';
 
 const Header = () => {
+  const date = moment().format('dddd, MMMM D, YYYY').toUpperCase(); 
+
   return (
     <header>
-      <img src={ coffeePic } alt='coffee cup' />
+      <p>{date}</p>
+      <img src={coffeePic} alt='coffee cup' />
     </header>
   );
 };
